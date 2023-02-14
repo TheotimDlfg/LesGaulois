d9fa8808ca3992f9c20b0cc4524d7dd15eb9898c
theotim <theotim@theotim>
2023-02-14 19:40:05 +0100

    Grand 1 petit 2 a TP1

---
+++ src/Gaulois.java
@@ -0,0 +1,42 @@
+
+
+public class Gaulois {
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	
+	@Override
+	public String toString() {
+	return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+		
+	public static void main(String[] args)
+	{
+		Gaulois asterix =  new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
+		System.out.println(asterix);
+	}
+}
