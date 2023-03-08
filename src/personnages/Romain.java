b8c3699ab11a43d13c6a2ec1cdf5c526e3d6647a
theotim <theotim.delforge@gmail.com>
2023-03-08 14:25:44 +0100

    FIN du TP2 avec les énumérations Equipement

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,14 +1,17 @@
-package personnages;
+package personnages; 
 
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement [] equipements;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) 
 	{
 		assert force > 0:"erreur la force du romain est négative ou nul";
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 	
 	public String getNom() {
@@ -34,8 +37,40 @@ public class Romain {
 		}		
 		assert this.force < f_debut: "La force n'as pas diminué";
 	}
+	
+	public void sEquiper(Equipement e) {
+		switch(nbEquipement){
+			case 2:
+				System.out.println("Le soldat " + nom +" est déjà bien protégé !");
+				break;
+			case 1:
+				if(equipements[0] == e)
+				{
+					System.out.println("Le soldat, " + nom + " possède déjà, un " + equipements[0] + " !");
+				}else {
+					nbEquipement++;
+					equipements[nbEquipement-1] = e;
+					System.out.println("Le soldat "+ nom +" s’équipe avec un " + e +".");
+				}
+				break;
+			default:
+				nbEquipement++;
+				equipements[0] = e;
+				System.out.println("Le soldat "+ nom +" s’équipe avec un " + e +".");
+				break;
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		Romain minus = new Romain("Minus", 6);
+		//System.out.println(Equipement.CASQUE);
+		//System.out.println(Equipement.BOUCLIER);
+		
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		
 	}
 }
